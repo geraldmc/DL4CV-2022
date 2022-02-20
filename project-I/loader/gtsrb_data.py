f6e1b9ae92ac63761697a84db483f9b79604be52
geraldmc <gerald.mccollam@gmail.com>
2022-02-20 17:25:51 -0600

    multiple changes

--- project-I/loader/gtsrb_data.py
+++ project-I/loader/gtsrb_data.py
@@ -10,17 +10,18 @@ from PIL import Image
 
 class GTSRB(Dataset):
 
-    def __init__(self, root_dir, transform=None):
+    def __init__(self, root_dir, transform=None, train=True):
         """
         """
         self.root_dir = root_dir
-        self.sub_directory = 'Training'
-        self.csv_file_name = 'GT_Training.csv'
-        #self.transform = transforms.Compose([transforms.ToTensor()])
+        if train:
+            self.sub_directory = 'Training'
+            self.csv_file_name = 'GT_Training.csv'
         self.transform = transform
 
-        csv_file_path = os.path.join(
-            root_dir, self.sub_directory, self.csv_file_name)
+        csv_file_path = os.path.join(self.root_dir, 
+                                     self.sub_directory, 
+                                     self.csv_file_name)
         
         self.csv_data = pd.read_csv(csv_file_path, sep=';')
 
@@ -28,9 +29,11 @@ class GTSRB(Dataset):
         return len(self.csv_data)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.root_dir, self.sub_directory,
+        img_path = os.path.join(self.root_dir, 
+                                self.sub_directory,
                                 self.csv_data.iloc[idx, 0])
         img = Image.open(img_path)
+        tensor_img = torch.from_numpy(np.array(img)) # see https://github.com/pytorch/vision/issues/2989
         #img = read_image(img_path)
 
         classId = self.csv_data.iloc[idx, 7]
@@ -39,45 +42,3 @@ class GTSRB(Dataset):
             img = self.transform(img)
 
         return img, classId
-    
-    
-'''
-class GTSRB(Dataset):
-    base_folder = 'GTSRB'
-
-    def __init__(self, root_dir, train=False, transform=None):
-        """
-        Args:
-            train (bool): Load trainingset or test set.
-            root_dir (string): Directory containing GTSRB folder.
-            transform (callable, optional): Optional transform to be applied
-                on a sample.
-        """
-        self.root_dir = root_dir
-
-        self.sub_directory = 'trainingset' if train else 'testset'
-        self.csv_file_name = 'training.csv' if train else 'test.csv'
-
-        csv_file_path = os.path.join(
-            root_dir, self.base_folder, self.sub_directory, self.csv_file_name)
-
-        self.csv_data = pd.read_csv(csv_file_path)
-
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.csv_data)
-
-    def __getitem__(self, idx):
-        img_path = os.path.join(self.root_dir, self.base_folder, self.sub_directory,
-                                self.csv_data.iloc[idx, 0])
-        img = Image.open(img_path)
-
-        classId = self.csv_data.iloc[idx, 1]
-
-        if self.transform is not None:
-            img = self.transform(img)
-
-        return img, classId
-        
-'''
