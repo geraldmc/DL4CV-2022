59165cf6f2b644fb45dc8ab7d02011fff1aa96ee
geraldmc <gerald.mccollam@gmail.com>
2022-03-10 10:16:47 -0600

    multiple changes

--- project-I/loader/transforms.py
+++ project-I/loader/transforms.py
@@ -12,7 +12,7 @@ data_transforms = transforms.Compose([
 data_jitter_contrast = transforms.Compose([
 	transforms.Resize((32, 32)),
     transforms.ColorJitter(contrast=5),
-    transforms.ColorJitter(contrast=-5),
+    transforms.ColorJitter(contrast=5),
     transforms.ToTensor(),
     transforms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629))
 ])
