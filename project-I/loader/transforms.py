a470407b0df89a73cb70ed991c8bc68e91bb2d40
geraldmc <gerald.mccollam@gmail.com>
2022-03-09 20:53:53 -0600

    multiple changes

---
+++ project-I/loader/transforms.py
@@ -0,0 +1,34 @@
+import os
+
+import torchvision.transforms as transforms
+
+data_transforms = transforms.Compose([
+    transforms.Resize((32, 32)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629))
+])
+
+# Resize, normalize and jitter image contrast
+data_jitter_contrast = transforms.Compose([
+	transforms.Resize((32, 32)),
+    transforms.ColorJitter(contrast=5),
+    transforms.ColorJitter(contrast=-5),
+    transforms.ToTensor(),
+    transforms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629))
+])
+
+# Resize, normalize and convert image to grayscale
+data_grayscale = transforms.Compose([
+    transforms.Resize((32, 32)),
+    transforms.Grayscale(num_output_channels=3),
+    transforms.ToTensor(),
+    transforms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629))
+])
+
+# Resize, normalize and translate
+data_translate = transforms.Compose([
+    transforms.Resize((32, 32)),
+    transforms.RandomAffine(degrees = 15,translate=(0.1,0.1)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629))
+])
