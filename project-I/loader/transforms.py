a0d8edfa70530e81f4a2c2d0ba738a639789b347
geraldmc <gerald.mccollam@gmail.com>
2022-03-11 22:34:07 -0600

    multiple changes

--- project-I/loader/transforms.py
+++ project-I/loader/transforms.py
@@ -8,15 +8,6 @@ data_transforms = transforms.Compose([
     transforms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629))
 ])
 
-# Resize, normalize and jitter image contrast
-data_jitter_contrast = transforms.Compose([
-	transforms.Resize((32, 32)),
-    transforms.ColorJitter(contrast=5),
-    transforms.ColorJitter(contrast=5),
-    transforms.ToTensor(),
-    transforms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629))
-])
-
 # Resize, normalize and convert image to grayscale
 data_grayscale = transforms.Compose([
     transforms.Resize((32, 32)),
@@ -29,6 +20,63 @@ data_grayscale = transforms.Compose([
 data_translate = transforms.Compose([
     transforms.Resize((32, 32)),
     transforms.RandomAffine(degrees = 15,translate=(0.1,0.1)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629))
+])
+
+# Resize, normalize and jitter image hues
+data_jitter_hue = transforms.Compose([
+	transforms.Resize((32, 32)),
+    transforms.ColorJitter(hue=0.4),
+    transforms.ToTensor(),
+    transforms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629))
+])
+
+# Resize, normalize and rotate image
+data_rotate = transforms.Compose([
+	transforms.Resize((32, 32)),
+    transforms.RandomRotation(15),
+    transforms.ToTensor(),
+    transforms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629))
+])
+
+# Resize, normalize and flip image horizontally and vertically
+data_hvflip = transforms.Compose([
+	transforms.Resize((32, 32)),
+    transforms.RandomHorizontalFlip(1),
+    transforms.RandomVerticalFlip(1),
+    transforms.ToTensor(),
+    transforms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629))
+])
+
+# Resize, normalize and flip image horizontally
+data_hflip = transforms.Compose([
+	transforms.Resize((32, 32)),
+    transforms.RandomHorizontalFlip(1),
+    transforms.ToTensor(),
+    transforms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629))
+])
+
+# Resize, normalize and flip image vertically
+data_vflip = transforms.Compose([
+	transforms.Resize((32, 32)),
+    transforms.RandomVerticalFlip(1),
+    transforms.ToTensor(),
+    transforms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629))
+])
+
+# Resize, normalize and shear image
+data_shear = transforms.Compose([
+	transforms.Resize((32, 32)),
+    transforms.RandomAffine(degrees = 15,shear=2),
+    transforms.ToTensor(),
+    transforms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629))
+])
+
+# Resize, normalize and crop image 
+data_center = transforms.Compose([
+	transforms.Resize((36, 36)),
+    transforms.CenterCrop(32),
     transforms.ToTensor(),
     transforms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629))
 ])
