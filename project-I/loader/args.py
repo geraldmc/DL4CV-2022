75d01f6f24e88f6e6ad578d02aec502be8689de8
geraldmc <gerald.mccollam@gmail.com>
2022-03-11 22:36:00 -0600

    multiple changes

--- project-I/loader/args.py
+++ project-I/loader/args.py
@@ -33,7 +33,7 @@ def get_args(local=False):
     
     parser.add_argument('--batch_size', type=int, default=50,
                        help='input batch size for training')
-    parser.add_argument('--num_epochs', type=int, default=5,
+    parser.add_argument('--num_epochs', type=int, default=8,
                        help='number of epochs to train')
     parser.add_argument('--learning_rate', type=float, default=0.0001,
                         help='learning rate (default: 0.0001)')
