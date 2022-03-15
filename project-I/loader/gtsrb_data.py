162dd236508318f16871150e631992dc86e0d6db
geraldmc <gerald.mccollam@gmail.com>
2022-03-15 11:09:59 -0500

    multiple changes

--- project-I/loader/gtsrb_data.py
+++ project-I/loader/gtsrb_data.py
@@ -1,14 +1,3 @@
-#import os
-#import torch
-#import torchvision
-#from torch.utils.data import Dataset
-#import torchvision.transforms as transforms
-#import torch.nn as nn
-#import torch.nn.functional as F
-#import torch.optim as optim 
-#import numpy as np
-#import pandas as pd
-
 import os
 import torch
 import pandas as pd
@@ -19,18 +8,20 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 from PIL import Image
 
+from loader.transforms import base_transform
+
 # See: https://debuggercafe.com/custom-dataset-and-dataloader-in-pytorch/
 
-class GTSRB(Dataset):
+class GTSRB_Test(Dataset):
 
-    def __init__(self, root_dir, transform=None, train=True):
+    def __init__(self, root_dir, test=True):
         """
         """
         self.root_dir = root_dir
-        if train:
-            self.sub_directory = 'training'
-            self.csv_file_name = 'GT_Training.csv'
-        self.transform = transform
+        if test:
+            self.sub_directory = 'Final_Test/Images/'
+            self.csv_file_name = 'GT-final_test.csv'
+        self.transform = base_transform
         self.frame = None
 
         csv_file_path = os.path.join(self.root_dir, 
@@ -38,11 +29,6 @@ class GTSRB(Dataset):
                                      self.csv_file_name)
         
         self.csv_data = pd.read_csv(csv_file_path, sep=';')
-        
-        #self.transform = transforms.Compose([transforms.Resize([256, 256]),
-        #                                     #transforms.RandomCrop(224),
-        #                                     #transforms.RandomHorizontalFlip(),
-        #                                     transforms.ToTensor()])
 
     def __len__(self):
         return len(self.csv_data)
@@ -54,12 +40,9 @@ class GTSRB(Dataset):
         img_frame_coords = (    self.csv_data.iloc[idx, 3:7]) # Roi.X1, Roi.Y1, Roi.X2, Roi.Y2
         
         img = Image.open(img_path)
-        tensor_img = torch.from_numpy(np.array(img)) # see https://github.com/pytorch/vision/issues/2989
-        #img = read_image(img_path)
-
         class_id = self.csv_data.iloc[idx, 7]
 
         if self.transform is not None:
             img = self.transform(img)
 
-        return img, class_id, img_frame_coords
+        return img, class_id
--- project-I/utils/tensor2image.py
+++ project-I/utils/tensor2image.py
@@ -1,19 +1,31 @@
-def tensor_to_image(tensor):
-    tensor = tensor*255
-    tensor = np.array(tensor, dtype=np.uint8)
-    if np.ndim(tensor)>3:
-        assert tensor.shape[0] == 1
-        tensor = tensor[0]
-    return PIL.Image.fromarray(tensor)
+import numpy as np
+from PIL import Image
+from torchvision import transforms
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
 
-def imshow(inp, title=None):
+
+def imshow(img, title=None):
     """Imshow for Tensor."""
-    inp = inp.numpy().transpose((1, 2, 0))
+    img = img.numpy().transpose((1, 2, 0))
     mean = np.array([0.485, 0.456, 0.406])
     std = np.array([0.229, 0.224, 0.225])
-    inp = std * inp + mean
-    inp = np.clip(inp, 0, 1)
-    plt.imshow(inp)
+    img = std * img + mean
+    img = np.clip(img, 0, 1)
+    plt.imshow(img)
     if title is not None:
         plt.title(title)
     plt.pause(0.001)  # pause a bit so that plots are updated
+    
+def imshow_frame(img, img_frame, tensor=False):
+    fig, ax = plt.subplots(1)
+    if tensor:
+        ax.imshow(img.permute(1, 2, 0))
+    else:
+        ax.imshow(img)
+    rect = patches.Rectangle((img_frame[0],img_frame[1]),
+                             img_frame[2]-img_frame[0],
+                             img_frame[3]-img_frame[1],
+                             linewidth=1, edgecolor='#00ff00',facecolor='none')
+    ax.add_patch(rect)
+    plt.show()
